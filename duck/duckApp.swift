72b2239313c94ef65d89104216df965fe90199e3
Felipe Passos <felipepassos@Felipes-MacBook-Pro.local>
2023-08-24 14:24:55 -0300

    thats all folks

---
+++ duck/duckApp.swift
@@ -0,0 +1,17 @@
+//
+//  duckApp.swift
+//  duck
+//
+//  Created by Felipe Passos on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct duckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
