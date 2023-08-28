5e61547b902e8bf2f8518f3f1ef44e75160aaa2e
Felipe Passos <felipepassos@Felipes-MacBook-Pro.local>
2023-08-28 16:25:09 -0300

    docs: document all services

--- duck/Services/StorageService.swift
+++ duck/Services/StorageService.swift
@@ -8,6 +8,19 @@
 import Foundation
 import SwiftUI
 
+/// A service responsible for managing storage-related operations, particularly those
+/// related to user preferences and settings.
+///
+/// This struct provides an encapsulated way to access and modify specific user-related
+/// settings using SwiftUI's `AppStorage` property wrapper, which automatically synchronizes
+/// with the user defaults system.
 struct StorageService {
+
+    /// Represents whether it's the user's first login.
+    ///
+    /// This property uses the `AppStorage` property wrapper to automatically read from and write to
+    /// the user defaults system. Whenever the value changes, the corresponding value in user defaults
+    /// is automatically updated.
     @AppStorage("isFirstLogin") static var isFirstLogin = false
 }
+
